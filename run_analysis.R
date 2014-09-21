665b71e7ac2ba5de0dfadb36fa518b5eff42a8b7
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2014-09-21 12:44:59 +0200

    Typo correction

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,7 @@ if (!file.exists("data")) {
     dir.create("data")
 }
 
-# Download and unzip the data file if it the data does not exist
+# Download and unzip the data file if the data does not exist
 datapath <- "./data/UCI HAR Dataset"
 if (!file.exists(datapath)) {
     fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
